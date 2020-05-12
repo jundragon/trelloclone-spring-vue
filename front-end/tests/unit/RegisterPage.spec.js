75aa7fae77daa0d81c7156920ab9f4733e26dd56
jundragon <jjundragon88@gmail.com>
2020-05-13 03:03:56 +0900

    단위테스트 vue-test-utils 로 리팩토링

--- front-end/tests/unit/RegisterPage.spec.js
+++ front-end/tests/unit/RegisterPage.spec.js
@@ -1,21 +1,44 @@
-import Vue from 'vue'
+import { mount } from '@vue/test-utils'
 import RegisterPage from '@/views/RegisterPage'
 
 describe('RegisterPage.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(RegisterPage)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.logo').getAttribute('src'))
+
+  let wrapper
+  let fieldUsername
+  let fieldEmailAddress
+  let fieldPassword
+  let buttonSubmit
+
+  beforeEach(() => {
+    wrapper = mount(RegisterPage)
+    fieldUsername = wrapper.find('#username')
+    fieldEmailAddress = wrapper.find('#emailAddress')
+    fieldPassword = wrapper.find('#password')
+    buttonSubmit = wrapper.find('form button[type="submit"]')
+  })
+
+  /**
+   * form 요소 검증 (화면 UI)
+   */
+  it('should render registration form', () => {
+    expect(wrapper.find('.logo').attributes().src)
       .toEqual('/images/logo.png')
-    expect(vm.$el.querySelector('.tagline').textContent)
+    expect(wrapper.find('.tagline').text())
       .toEqual('Open source task management tool')
-    expect(vm.$el.querySelector('#username').value)
+    expect(fieldUsername.element.value)
       .toEqual('')
-    expect(vm.$el.querySelector('#emailAddress').value)
+    expect(fieldEmailAddress.element.value)
       .toEqual('')
-    expect(vm.$el.querySelector('#password').value)
+    expect(fieldPassword.element.value)
       .toEqual('')
-    expect(vm.$el.querySelector('form button[type="submit"]').textContent)
+    expect(buttonSubmit.text())
       .toEqual('Create account')
   })
+
+  /**
+   * 데이터 모델 초깃값 테스트
+   * 데이터 모데로가 폼 입력 필드간의 바인딩 테스트
+   * 폼 이벤트 핸들러의 존재 여부 테스트
+   */
+
 })
