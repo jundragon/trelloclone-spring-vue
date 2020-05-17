335c70adb82389e59f9459182d045591a08047d4
jundragon <jjundragon88@gmail.com>
2020-05-18 02:04:10 +0900

    fix: e2e 테스트 오류 수정

--- front-end/tests/e2e/page-objects/HomePage.js
+++ front-end/tests/e2e/page-objects/HomePage.js
@@ -3,10 +3,8 @@ module.exports = {
     return this.api.launchUrl
   },
   elements: {
-    pageTitle: {
-      logoImage: {
-        selector: '.logo img'
-      }
+    logoImage: {
+      selector: '.logo img'
     }
   }
 }
