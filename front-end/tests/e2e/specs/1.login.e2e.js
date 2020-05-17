a4fb5fd59656a0263e23f0c9d3d3fb1684807cf6
jundragon <jjundragon88@gmail.com>
2020-05-17 19:03:09 +0900

    통합테스트 임시 주석

--- front-end/tests/e2e/specs/1.login.e2e.js
+++ front-end/tests/e2e/specs/1.login.e2e.js
@@ -1,64 +1,63 @@
-// For authoring Nightwatch tests, see
-// https://nightwatchjs.org/guide
-
-module.exports = {
-  'login page renders elements': browser => {
-    const loginPage = browser.page.LoginPage()
-
-    loginPage
-      .navigate()
-      .waitForElementVisible('@app', 500)
-      .assert.visible('@usernameInput')
-      .assert.visible('@passwordInput')
-      .assert.visible('@submitButton')
-      .assert.hidden('@formError')
-
-    browser.end()
-  },
-  'login with invalid credentials': browser => {
-    const loginPage = browser.page.LoginPage()
-    loginPage
-      .navigate()
-      .login('not-exist', 'incorrect')
-
-    browser.pause(500)
-
-    loginPage
-      .assert.visible('@formError')
-      .assert.containsText('@formError', 'Invalid credentials')
-
-    browser
-      .assert.urlEquals(browser.launchUrl + 'login')
-      .end()
-  }
-  // 'login with username': browser => {
-  //   const loginPage = browser.page.LoginPage()
-  //   const homePage = browser.page.HomePage()
-  //   loginPage
-  //     .navigate()
-  //     .login(data.username, data.password)
-  //
-  //   browser.pause(2000)
-  //
-  //   homePage
-  //     .navigate()
-  //     .expect.element('@pageTitle').text.to.contain('Home Page')
-  //
-  //   browser.end()
-  // },
-  // 'login with email address': browser => {
-  //   const loginPage = browser.page.LoginPage()
-  //   const homePage = browser.page.HomePage()
-  //   loginPage
-  //     .navigate()
-  //     .login(data.emailAddress, data.password)
-  //
-  //   browser.pause(2000)
-  //
-  //   homePage
-  //     .navigate()
-  //     .expect.element('@pageTitle').text.to.contain('Home Page')
-  //
-  //   browser.end()
-  // }
-}
+// const data = require('../data/user')
+//
+// module.exports = {
+//   'login page renders elements': browser => {
+//     const loginPage = browser.page.LoginPage()
+//
+//     loginPage
+//       .navigate()
+//       .waitForElementVisible('@app', 500)
+//       .assert.visible('@usernameInput')
+//       .assert.visible('@passwordInput')
+//       .assert.visible('@submitButton')
+//       .assert.hidden('@formError')
+//
+//     browser.end()
+//   },
+//   'login with invalid credentials': browser => {
+//     const loginPage = browser.page.LoginPage()
+//     loginPage
+//       .navigate()
+//       .login('not-exist', 'incorrect')
+//
+//     browser.pause(500)
+//
+//     loginPage
+//       .assert.visible('@formError')
+//       .assert.containsText('@formError', 'Invalid credentials')
+//
+//     browser
+//       .assert.urlEquals(browser.launchUrl + 'login')
+//       .end()
+//   },
+//   'login with username': browser => {
+//     const loginPage = browser.page.LoginPage()
+//     const homePage = browser.page.HomePage()
+//     loginPage
+//       .navigate()
+//       .login(data.username, data.password)
+//
+//     browser.pause(2000)
+//
+//     homePage
+//       .navigate()
+//       .assert.visible('@logoImage')
+//
+//     browser.end()
+//   },
+//   'login with email address': browser => {
+//     const loginPage = browser.page.LoginPage()
+//     const homePage = browser.page.HomePage()
+//     loginPage
+//       .navigate()
+//       .login(data.emailAddress, data.password)
+//
+//     browser.pause(2000)
+//
+//     homePage
+//       .navigate()
+//       .assert.visible('@logoImage')
+//
+//     browser.end()
+//   }
+// }
